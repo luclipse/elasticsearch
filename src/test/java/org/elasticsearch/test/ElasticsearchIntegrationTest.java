58f0da65ac90b16ff8b7ee48ffce218b59e98b1c
Boaz Leskes <b.leskes@gmail.com>
2014-05-16 22:09:39 +0200

    Added ServiceDisruptionScheme(s) and testAckedIndexing
    This commit adds the notion of ServiceDisruptionScheme allowing for introducing disruptions in our test cluster. This
    abstraction as used in a couple of wrappers around the functionality offered by MockTransportService to simulate various
    network partions. There is also one implementation for causing a node to be slow in processing cluster state updates.
    
    This new mechnaism is integrated into existing tests DiscoveryWithNetworkFailuresTests.
    
    A new test called testAckedIndexing is added to verify retrieval of documents whose indexing was acked during various disruptions.
    
    Closes #6505

--- src/test/java/org/elasticsearch/test/ElasticsearchIntegrationTest.java
+++ src/test/java/org/elasticsearch/test/ElasticsearchIntegrationTest.java
@@ -93,6 +93,7 @@ import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.search.SearchService;
 import org.elasticsearch.test.client.RandomizingClient;
 import org.hamcrest.Matchers;
+import org.elasticsearch.test.disruption.ServiceDisruptionScheme;
 import org.junit.*;
 
 import java.io.IOException;
@@ -518,6 +519,7 @@ public abstract class ElasticsearchIntegrationTest extends ElasticsearchTestCase
         boolean success = false;
         try {
             logger.info("[{}#{}]: cleaning up after test", getTestClass().getSimpleName(), getTestName());
+            clearDisruptionScheme();
             final Scope currentClusterScope = getCurrentClusterScope();
             try {
                 if (currentClusterScope != Scope.TEST) {
@@ -629,6 +631,15 @@ public abstract class ElasticsearchIntegrationTest extends ElasticsearchTestCase
 
     protected int numberOfReplicas() {
         return between(minimumNumberOfReplicas(), maximumNumberOfReplicas());
+    }
+
+
+    public void setDisruptionScheme(ServiceDisruptionScheme scheme) {
+        internalCluster().setDisruptionScheme(scheme);
+    }
+
+    public void clearDisruptionScheme() {
+        internalCluster().clearDisruptionScheme();
     }
 
     /**
